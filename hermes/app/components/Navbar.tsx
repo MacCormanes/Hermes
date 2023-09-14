3ea0d465da97c9ae80e28c515ec5b0e1d99c42c6
MacCormanes <janmarc132@gmail.com>
2023-09-15 00:48:35 +0800

    Added Avatar functionality with dropdown and fixed signOut feature

--- hermes/app/components/Navbar.tsx
+++ hermes/app/components/Navbar.tsx
@@ -8,33 +8,67 @@ import {
   SheetHeader,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { createUserDocumentFromAuth, getCategoriesAndDocuments, onAuthStateChangedListener, signOutUser } from "@/firebase/firebase.utils";
+import {
+  auth,
+  getCategoriesAndDocuments,
+  signOutUser,
+} from "@/firebase/firebase.utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import CartDropdown from "./ui/CartDropdown";
 import { useAppDispatch, useAppSelector } from "../store/store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setCategoriesMap } from "../rtk-slices/categoriesSlice";
 import { fetchUserCart } from "../rtk-slices/cartSlice";
+import { Avatar } from "@radix-ui/react-avatar";
+import { AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
 
 const Navbar = () => {
-  const currentUser = useAppSelector((state) => state.user.currentUser)
-  const dispatch = useAppDispatch()
+  const currentUser = useAppSelector((state) => state.user.currentUser);
+  const dispatch = useAppDispatch();
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  const userData = auth.currentUser?.providerData[0];
+  const userAvatar = userData?.photoURL;
+  const name = userData?.displayName;
+
+  const inputString = name;
+  const words = inputString?.split(" ");
+  let firstName = "";
+  if (words) {
+    firstName = words[0];
+  }
+
+  const handleSignOut = async () => {
+    await signOutUser();
+    window.location.reload();
+  };  
 
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap)
+      console.log(categoryMap);
       dispatch(setCategoriesMap(categoryMap));
     };
     getCategoriesMap();
   }, []);
 
-
- useEffect(() => {
-  dispatch(fetchUserCart())
-  }, [])
+  useEffect(() => {
+    dispatch(fetchUserCart());
+  }, []);
 
   useEffect(() => {
-    }, [currentUser])
+    if (currentUser) {
+      setisLoggedIn(true);
+    } else {
+      setisLoggedIn(false);
+    }
+  }, [currentUser]);
 
   return (
     <div className="flex items-center justify-between p-0 m-0 bg-orange-200 font-montserrat text-orange-950 h-[80px] min-w-full">
@@ -104,20 +138,50 @@ const Navbar = () => {
           <span className="">Shop</span>
         </Link>
       </div>
-      <div className="flex items-center justify-end gap-4">
-        {
-        currentUser ? (
-          <button className="" onClick={signOutUser}>
-            Sign Out
-          </button>
-        ) : (
-          <Link href="/signIn">
-            <span className="">Sign In</span>
-          </Link>
-        )}
+      <div className="flex items-center justify-end gap-1">
         <div className="">
           <CartDropdown />
         </div>
+        {isLoggedIn ? (
+          <>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar>
+                  <AvatarImage
+                    src={`${userAvatar}`}
+                    className="mr-10 rounded-full w-11 h-11"
+                  />
+                  <AvatarFallback className="bg-white rounded-full w-11 h-11"></AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg-orange-100">
+                <DropdownMenuLabel className="text-base text-orange-900">
+                  {firstName}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="border-hidden hover:border-hidden">
+                  <Link
+                    href={"/shop/checkout"}
+                    className="pt-2 pb-1 text-sm transition-all duration-500 text-orange-950 hover:text-orange-700 hover:border-hidden border-hidden"
+                  >
+                    Go To Checkout
+                  </Link>
+                </DropdownMenuItem>
+                <button
+                  type="button"
+                  className="py-1 text-sm transition-all duration-500 text-orange-950 hover:text-orange-700"
+                  onClick={handleSignOut}
+                >
+                  Sign Out
+                </button>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        ) : (
+          <Link href="/signIn">
+            <span className="mr-10">Sign In</span>
+          </Link>
+        )}
       </div>
     </div>
   );
